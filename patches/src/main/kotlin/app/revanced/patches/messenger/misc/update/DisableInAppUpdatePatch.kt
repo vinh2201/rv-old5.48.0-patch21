3ad9b3a96b5e664f599d6704596cb228a5daa9c9
Nguyễn Tấn Vĩnh <potter.harry0909@yahoo.com>
2026-07-24 11:07:18 +0700

    Test

--- patches/src/main/kotlin/app/revanced/patches/messenger/misc/update/DisableInAppUpdatePatch.kt
+++ patches/src/main/kotlin/app/revanced/patches/messenger/misc/update/DisableInAppUpdatePatch.kt
@@ -3,10 +3,15 @@ package app.revanced.patches.messenger.misc.update
 import app.revanced.patcher.extensions.InstructionExtensions.replaceInstruction
 import app.revanced.patcher.fingerprint
 import app.revanced.patcher.patch.bytecodePatch
+import com.android.tools.smali.dexlib2.Opcode
 
-// Using old fingerprint structures to find constructors
 internal val inAppUpdaterConstructorFingerprint = fingerprint {
     returns("V")
+    // Bổ sung opcodes để ngăn Patcher quét toàn bộ APK gây lỗi ngầm
+    opcodes(
+        Opcode.INVOKE_DIRECT,
+        Opcode.INVOKE_STATIC
+    )
     custom { method, _ ->
         method.name == "<init>" &&
         method.definingClass == "Lcom/facebook/messenger/app/update/InAppUpdater;"
@@ -16,13 +21,12 @@ internal val inAppUpdaterConstructorFingerprint = fingerprint {
 @Suppress("unused")
 val disableInAppUpdatePatch = bytecodePatch(
     name = "Disable in-app update",
-    description = "Disable in-app update notification in Facebook Messenger app.",
+    // Dùng tiếng Anh để tránh lỗi decode UTF-8 trên Windows CMD
+    description = "Disables the in-app update check mechanism.",
 ) {
     compatibleWith("com.facebook.orca")
 
     execute {
-        // 0 to call super.<init>() force system use.
-        // 1 to add return-void to break the entire check update string.
         inAppUpdaterConstructorFingerprint.methodOrNull?.replaceInstruction(1, "return-void")
     }
 }
