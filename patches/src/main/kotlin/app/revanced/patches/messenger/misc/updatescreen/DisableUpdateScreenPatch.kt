efa5f5ecf5378eec9810c7959cf78367a4c38af3
Nguyễn Tấn Vĩnh <potter.harry0909@yahoo.com>
2026-07-26 10:56:48 +0700

    test

--- patches/src/main/kotlin/app/revanced/patches/messenger/misc/updatescreen/DisableUpdateScreenPatch.kt
+++ patches/src/main/kotlin/app/revanced/patches/messenger/misc/updatescreen/DisableUpdateScreenPatch.kt
@@ -6,24 +6,38 @@ import app.revanced.patcher.patch.bytecodePatch
 import app.revanced.patcher.util.proxy.mutableTypes.MutableMethod.Companion.toMutable
 
 internal val inAppUpdateStringFingerprint = fingerprint {
-    strings("InAppUpdater")
+    strings("InAppUpdater.checkUpdateAvailability")
+}
+
+internal val appUpdateServiceStringFingerprint = fingerprint {
+    strings("AppUpdateService")
 }
 
 @Suppress("unused")
 val disableInAppUpdatePatch = bytecodePatch(
     name = "Disable in-app update",
-    description = "Disables Messenger in-app update checks.",
+    description = "Disables Messenger in-app update checks and Google Play update service hooks.",
 ) {
     compatibleWith("com.facebook.orca")
 
     execute {
-        val targetMethod = inAppUpdateStringFingerprint.method.toMutable()
-        
-        when (targetMethod.returnType) {
-            "V" -> targetMethod.replaceInstruction(0, "return-void")
+        // Vô hiệu hóa hàm kiểm tra cập nhật nội bộ của Messenger
+        val updateMethod = inAppUpdateStringFingerprint.method.toMutable()
+        when (updateMethod.returnType) {
+            "V" -> updateMethod.replaceInstruction(0, "return-void")
             "Z" -> {
-                targetMethod.replaceInstruction(0, "const/4 v0, 0x0")
-                targetMethod.replaceInstruction(1, "return v0")
+                updateMethod.replaceInstruction(0, "const/4 v0, 0x0")
+                updateMethod.replaceInstruction(1, "return v0")
+            }
+        }
+
+        // Vô hiệu hóa dịch vụ yêu cầu cập nhật qua Google Play Core
+        val serviceMethod = appUpdateServiceStringFingerprint.method.toMutable()
+        when (serviceMethod.returnType) {
+            "V" -> serviceMethod.replaceInstruction(0, "return-void")
+            "Z" -> {
+                serviceMethod.replaceInstruction(0, "const/4 v0, 0x0")
+                serviceMethod.replaceInstruction(1, "return v0")
             }
         }
     }
