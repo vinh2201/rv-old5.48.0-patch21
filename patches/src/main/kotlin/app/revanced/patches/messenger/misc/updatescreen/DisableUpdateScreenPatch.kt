c06ec7c8514cf164be4ef08bd53fd33ae6f7ce23
Nguyễn Tấn Vĩnh <potter.harry0909@yahoo.com>
2026-07-26 13:41:32 +0700

    test

--- patches/src/main/kotlin/app/revanced/patches/messenger/misc/updatescreen/DisableUpdateScreenPatch.kt
+++ patches/src/main/kotlin/app/revanced/patches/messenger/misc/updatescreen/DisableUpdateScreenPatch.kt
@@ -1,48 +1,35 @@
 package app.revanced.patches.messenger.misc.updatescreen
 
-import app.revanced.patcher.extensions.InstructionExtensions.addInstructions
-import app.revanced.patcher.fingerprint
-import app.revanced.patcher.patch.bytecodePatch
-import app.revanced.patcher.util.proxy.mutableTypes.MutableMethod.Companion.toMutable
-
-internal val versionUpgradeRequiredFingerprint = fingerprint {
-    strings("Setting versionUpgradeRequired = ")
-}
-
-internal val armadilloUpgradeBlockerFingerprint = fingerprint {
-    strings("armadillo_app_upgrade_screen_blocker")
-}
-
-internal val linkUpgradeVersionFingerprint = fingerprint {
-    strings("link_upgrade_version")
-}
+import app.revanced.patcher.patch.resourcePatch
+import org.w3c.dom.Element
 
 @Suppress("unused")
-val disableInAppUpdatePatch = bytecodePatch(
+val disableInAppUpdatePatch = resourcePatch(
     name = "Disable in-app update",
-    description = "Forces update checks and upgrade blockers to return false immediately.",
+    description = "Blocks the update screen during calls by disabling the MsgrRUPBlockActivity in AndroidManifest.",
 ) {
     compatibleWith("com.facebook.orca")
 
-    execute {
-        val targets = listOf(
-            versionUpgradeRequiredFingerprint,
-            armadilloUpgradeBlockerFingerprint,
-            linkUpgradeVersionFingerprint
-        )
-
-        for (target in targets) {
-            val method = target.method.toMutable()
-            when (method.returnType) {
-                "V" -> method.addInstructions(0, "return-void")
-                "Z", "I" -> {
-                    method.addInstructions(
-                        0,
-                        """
-                        const/4 v0, 0x0
-                        return v0
-                        """
-                    )
+    finalize {
+        document("AndroidManifest.xml").use { document ->
+            // Lấy danh sách toàn bộ các thẻ <activity> trong Manifest
+            val activities = document.getElementsByTagName("activity")
+            
+            for (i in 0 until activities.length) {
+                val activity = activities.item(i) as Element
+                val activityName = activity.getAttribute("android:name")
+                
+                // Chỉ điểm đúng thủ phạm chặn cuộc gọi
+                if (activityName == "com.facebook.rtc.activities.upgradepolicy.msgr.MsgrRUPBlockActivity") {
+                    // Chốt hạ: Vô hiệu hóa hoàn toàn Activity này
+                    activity.setAttribute("android:enabled", "false")
+                    
+                    // Ép thêm phát nữa: Rút luôn quyền gọi từ bên ngoài (nếu có)
+                    if (activity.hasAttribute("android:exported")) {
+                        activity.setAttribute("android:exported", "false")
+                    }
+                    
+                    break // Tóm được rồi thì ngắt vòng lặp cho nhẹ build
                 }
             }
         }
