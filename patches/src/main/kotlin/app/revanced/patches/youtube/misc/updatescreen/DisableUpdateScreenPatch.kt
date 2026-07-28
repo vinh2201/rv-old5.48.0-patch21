549c2292a41b57b9a4b09fb9c3fbd34d0b7d54c9
Nguyễn Tấn Vĩnh <potter.harry0909@yahoo.com>
2026-07-29 00:28:24 +0700

    test

---
+++ patches/src/main/kotlin/app/revanced/patches/youtube/misc/updatescreen/DisableUpdateScreenPatch.kt
@@ -0,0 +1,36 @@
+package app.revanced.patches.youtube.general.updatescreen
+
+import app.revanced.patcher.extensions.InstructionExtensions.addInstructions
+import app.revanced.patcher.patch.bytecodePatch
+import app.revanced.util.fingerprint.mutableClassOrThrow
+import com.android.tools.smali.dexlib2.util.MethodUtil
+
+internal val appBlockingCheckResultToStringFingerprint = fingerprint(
+    name = "appBlockingCheckResultToStringFingerprint",
+    returnType = "Ljava/lang/String;",
+    strings = listOf("AppBlockingCheckResult{intent=")
+)
+
+val disableUpdateScreen = bytecodePatch(
+    name = "Disable update screen",
+    description = "Disable the force update screen (\"Switch to YouTube.com\" or \"Update your app\").",
+) {
+    compatibleWith(
+        "com.google.android.youtube"(
+            "19.34.42",
+            "20.07.39",
+            "20.13.41",
+            "20.14.43",
+        )
+    )
+
+    execute {
+        appBlockingCheckResultToStringFingerprint.mutableClassOrThrow().methods.first { method ->
+            MethodUtil.isConstructor(method) &&
+                    method.parameters == listOf("Landroid/content/Intent;", "Z")
+        }.addInstructions(
+            1,
+            "const/4 p1, 0x0"
+        )
+    }
+}
